8bd6f4ecc76260d140655e31e696053e71aa00be
Jonathan Gomes Selman <jonathan@rungalileo.io>
2023-08-07 11:00:41 -0400

    switching datasets

--- example_scripts/test_with_galileo.py
+++ example_scripts/test_with_galileo.py
@@ -2,7 +2,6 @@ import sys
 import os
 import numpy as np
 
-
 sys.path.insert(0, "src")
 
 from refined.dataset_reading.entity_linking.dataset_factory import Datasets
@@ -13,17 +12,17 @@ from refined.evaluation.evaluation import evaluate, get_datasets_obj
 
 import dataquality as dq
 
-
 USE_ALL_ENTITY_TYPES = True
 GALILEO_PROJECT_NAME = "MusixMatchFull"
 BATCH_SIZE = 100
+PODCAST_ENG_SPLIT = "dev"
 
 
 def run_batch_inference_job(
-    refined: Refined,
-    datasets: Datasets,
-    fine_tuning_args: FineTuningArgs,
-    run_name: str
+        refined: Refined,
+        datasets: Datasets,
+        fine_tuning_args: FineTuningArgs,
+        run_name: str
 ) -> None:
     dq.init(task_type="text_multi_label",
             project_name=GALILEO_PROJECT_NAME,
@@ -36,7 +35,7 @@ def run_batch_inference_job(
     evaluation_dataset_name_to_docs = {
         "PODCAST-ENG": list(datasets.get_podcast_docs(
             # TODO Specify split
-            split="dev"
+            split=PODCAST_ENG_SPLIT
         ))
     }
     print("Number of documents:", len(evaluation_dataset_name_to_docs['AIDA']))
@@ -47,12 +46,13 @@ def run_batch_inference_job(
     # Run the model over the documents using the evaluate function
     dq.set_split("test")
     dq.set_epoch(0)
-    evaluate(refined=refined,
-             evaluation_dataset_name_to_docs=evaluation_dataset_name_to_docs,
-             el=False,  # only evaluate EL when training EL
-             ed=True,  # always evaluate standalone ED
-             ed_threshold=fine_tuning_args.ed_threshold
-             )
+    evaluate(
+        refined=refined,
+        evaluation_dataset_name_to_docs=evaluation_dataset_name_to_docs,
+        el=False,  # No need to eval EL
+        ed=True,
+        ed_threshold=fine_tuning_args.ed_threshold
+    )
 
     dq.finish()
 
@@ -70,12 +70,11 @@ def main():
                                       use_precomputed_descriptions=fine_tuning_args.use_precomputed_descriptions,
                                       device=fine_tuning_args.device)
 
-    print("Data dir:", fine_tuning_args.data_dir)
     datasets = get_datasets_obj(preprocessor=refined.preprocessor,
                                 download=False,
                                 data_dir=fine_tuning_args.data_dir)
 
-    # Extract the entity ids that we are logging with Galileo.
+    # 🔭🌕 Galileo Extract the entity ids that we are logging with Galileo.
     entity_type_ids = refined.preprocessor.lookups.label_subset_arr
     if USE_ALL_ENTITY_TYPES:
         # Create the list of all the entity type ids
@@ -83,14 +82,17 @@ def main():
 
     # 🔭🌕 Galileo - Batch processing of the entity-types.
     for i, batch_start_idx in enumerate(range(0, entity_type_ids.shape[0], BATCH_SIZE)):
-        print (f"Starting Batch {i} with entity id range {entity_type_ids[batch_start_idx]} - {entity_type_ids[batch_start_idx + BATCH_SIZE - 1]}")
+        print(
+            f"Starting Batch {i} with entity id range {entity_type_ids[batch_start_idx]} "
+            f"- {entity_type_ids[batch_start_idx + BATCH_SIZE - 1]}"
+        )
         entity_types_subset = entity_type_ids[batch_start_idx: batch_start_idx + BATCH_SIZE]
 
         # Track just the entity types in the current batch
         refined.preprocessor.lookups.label_subset_arr = entity_types_subset
         refined.preprocessor.lookups.label_subset_set = set(entity_types_subset)
 
-        # Create a Galileo Inference Job
+        # Launch a Galileo Test Job
         run_name = f"Batch_Run_{i}"
         run_batch_inference_job(
             refined,
